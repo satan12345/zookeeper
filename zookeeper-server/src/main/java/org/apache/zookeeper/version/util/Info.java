82c170ee4790047d06a2cb41f7625d8dd36a1647
jipeng <jipeng@able-elec.com>
2021-04-12 17:03:39 +0800

    首次编译

---
+++ zookeeper-server/src/main/java/org/apache/zookeeper/version/util/Info.java
@@ -0,0 +1,16 @@
+package org.apache.zookeeper.version.util;
+
+/**
+ * @author jipeng
+ * @date 2021-04-09 18:50
+ * @description
+ */
+public interface Info {
+    int MAJOR = 1;
+    int MINOR = 0;
+    int MICRO = 0;
+    String QUALIFIER = null;
+    int REVISION = -1;
+    String REVISION_HASH = "1";
+    String BUILD_DATE = "2020-10-15";
+}
